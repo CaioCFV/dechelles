95236ceaacf77c1931b3b124afe4e80f1149b57b
Vieira <caio2011dhd@gmail.com>
2025-02-26 14:49:09 -0300

    nova fonte

---
+++ app-custom/react/components/SliderProduct/components/Zoom.tsx
@@ -0,0 +1,81 @@
+import React from 'react'
+import { applyModifiers, useCssHandles } from 'vtex.css-handles'
+
+export const HANDLES_SLIDER = [
+  'slider__zoom',
+  'slider__zoom--image',
+  'slider__zoom--close',
+  'slider__zoom--wrapper',
+] as const
+
+type ZoomProps = {
+  isActive: boolean
+  disableZoom: React.MouseEventHandler
+  images: { imageId: string }[]
+  index: number
+}
+
+export function Zoom({ isActive, disableZoom, images, index }: ZoomProps) {
+  const { handles } = useCssHandles(HANDLES_SLIDER)
+
+  return (
+    <>
+      <div
+        className={applyModifiers(
+          handles.slider__zoom,
+          isActive ? 'active' : ''
+        )}
+        onClick={disableZoom}
+      >
+        <button
+          className={handles['slider__zoom--close']}
+          onClick={disableZoom}
+        >
+          Fechar
+          <img
+            src={require('../images/icon-close.png')}
+            alt="icon-close"
+            width={12}
+            style={{ marginLeft: '10px' }}
+          />
+        </button>
+        <div className={handles['slider__zoom--wrapper']}>
+          {isActive &&
+            images.map(function ({ imageId }, indice) {
+              if (indice == index) {
+                return (
+                  <>
+                    <img
+                      src={
+                        `https://michaelkorsbr.vtexassets.com/arquivos/ids/` +
+                        imageId +
+                        '/produto.webp'
+                      }
+                      alt="Imagem zoom"
+                      className={
+                        handles['slider__zoom--image'] + ' vtex-current-image'
+                      }
+                    />
+                  </>
+                )
+              }
+
+              return (
+                <>
+                  <img
+                    src={
+                      `https://michaelkorsbr.vtexassets.com/arquivos/ids/` +
+                      imageId +
+                      '/produto.webp'
+                    }
+                    alt="Imagem zoom"
+                    className={handles['slider__zoom--image']}
+                  />
+                </>
+              )
+            })}
+        </div>
+      </div>
+    </>
+  )
+}
---
+++ app-custom/react/components/SliderProduct/index.tsx
@@ -0,0 +1,217 @@
+import React, { useState } from 'react'
+import { useProduct } from 'vtex.product-context'
+import { applyModifiers, useCssHandles } from 'vtex.css-handles'
+import Slider from 'react-slick'
+import { canUseDOM, useRuntime } from 'vtex.render-runtime'
+import './styles/slider.css'
+import { Zoom } from './components/Zoom'
+
+export const HANDLES_SLIDER = [
+  'slider',
+  'slider__thumbs',
+  'slider__thumbs__wrapper',
+  'slider__thumbs__slide',
+  'slider__thumbs__image',
+  'slider__thumbs__prev',
+  'slider__thumbs__next',
+  'slider__wrapper',
+  'slider__main',
+  'slider__main__slider',
+  'slider__main__image',
+  'slider__next',
+  'slider__prev',
+  'slider__dots',
+] as const
+
+export function SliderProduct() {
+  const product: any = useProduct()
+  const { handles } = useCssHandles(HANDLES_SLIDER)
+  const { deviceInfo } = useRuntime()
+  const maxImages = product.selectedItem.images.length - 1
+  const [current, setIndex] = useState(0)
+  const [zoom, setZoom] = useState({
+    isActive: false,
+    index: -1,
+  })
+  function doScrolling(elementY: any, duration: any) {
+    var startingY = window.pageYOffset
+    var diff = elementY - startingY + 50
+    var start: any
+    window.requestAnimationFrame(function step(timestamp) {
+      if (!start) start = timestamp
+      var time = timestamp - start
+      var percent = Math.min(time / duration, 1)
+      window.scrollTo(0, startingY + diff * percent)
+      if (time < duration) {
+        window.requestAnimationFrame(step)
+      }
+    })
+  }
+
+  const scrollToImage = function (index: number) {
+    const elmnt: any = document.querySelector(`[data-image="${index}"]`)
+    doScrolling(elmnt.offsetTop, 200)
+  }
+
+  const next = () => {
+    let val = current + 1
+    if (val > maxImages) val = 0
+    setIndex(val)
+    scrollToImage(val)
+  }
+
+  const setImage = (index: any) => {
+    setIndex(index)
+    scrollToImage(index)
+  }
+
+  const previous = () => {
+    let val = current - 1
+    if (val < 0) val = maxImages
+    setIndex(val)
+    scrollToImage(val)
+  }
+
+  if (deviceInfo.isMobile) {
+    const settings = {
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      infinite: true,
+      arrows: false,
+      dots: true,
+    }
+    return (
+      <>
+        <Slider {...settings} className={handles.slider__main}>
+          {product.selectedItem.images.map(function (item: any, index: any) {
+            return (
+              <div
+                key={item.imageId}
+                className={handles.slider__main__slider}
+                onClick={() => {
+                  setIndex(index)
+                  setZoom({ ...zoom, isActive: true, index: index })
+                }}
+              >
+                <img
+                  src={
+                    `https://michaelkorsbr.vtexassets.com/arquivos/ids/` +
+                    item.imageId +
+                    '-710-1065/produto.webp'
+                  }
+                  className={handles.slider__main__image}
+                />
+              </div>
+            )
+          })}
+        </Slider>
+        <Zoom
+          isActive={zoom.isActive}
+          images={product.selectedItem.images}
+          disableZoom={() => setZoom({ ...zoom, isActive: false })}
+          index={zoom.index}
+        />
+      </>
+    )
+  }
+
+  const settingsTHUMBS = {
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    vertical: true,
+    verticalSwiping: true,
+    infinite: false,
+    arrows: false,
+    dots: false,
+    draggable: true,
+  }
+
+  if (canUseDOM) {
+    document
+      .querySelector('[class*="-product__info--addtocart"] .vtex-button')
+      ?.addEventListener('click', function () {
+        setTimeout(function () {
+          document
+            .querySelector('[class*="minicartIconContainer--header__minicart"]')
+            //@ts-ignore
+            ?.click()
+        }, 4000)
+      })
+  }
+
+  return (
+    <div className={handles.slider}>
+      <div className={handles.slider__thumbs__wrapper}>
+        <Slider {...settingsTHUMBS} className={handles.slider__thumbs}>
+          {product.selectedItem.images.map(function (item: any, index: number) {
+            return (
+              <div
+                key={item.imageId}
+                className={applyModifiers(
+                  handles.slider__thumbs__slide,
+                  index == current ? 'active' : ''
+                )}
+                onClick={() => setImage(index)}
+              >
+                <img
+                  src={
+                    `https://michaelkorsbr.vtexassets.com/arquivos/ids/` +
+                    item.imageId +
+                    '-70-100/produto.webp'
+                  }
+                  className={handles.slider__thumbs__image}
+                />
+              </div>
+            )
+          })}
+        </Slider>
+        <button
+          className={handles.slider__thumbs__prev}
+          onClick={() => previous()}
+        >
+          <img
+            //src={require('./images/large-arrow.svg')}
+            alt="próximo slide"
+            style={{ width: '42px', margin: '0 auto' }}
+          />
+        </button>
+        <button className={handles.slider__thumbs__next} onClick={() => next()}>
+          <img
+            //src={require('./images/large-arrow.svg')}
+            alt="slide anterior"
+            style={{ width: '42px', margin: '0 auto' }}
+          />
+        </button>
+      </div>
+      <div className={handles.slider__main}>
+        {product.selectedItem.images.map(function (item: any, index: number) {
+          return (
+            <div
+              key={item.imageId}
+              className={handles.slider__main__slider}
+              data-image={index}
+            >
+              <img
+                src={
+                  `https://michaelkorsbr.vtexassets.com/arquivos/ids/` +
+                  item.imageId +
+                  '-710-1065/produto.webp'
+                }
+                className={handles.slider__main__image}
+                onClick={() => {
+                  setZoom({ ...zoom, isActive: true, index: index })
+                }}
+              />
+            </div>
+          )
+        })}
+      </div>
+      <Zoom
+        isActive={zoom.isActive}
+        images={product.selectedItem.images}
+        disableZoom={() => setZoom({ ...zoom, isActive: false })}
+        index={zoom.index}
+      />
+    </div>
+  )
+}
